33e8cad5d41518756d265dd0f45a147b95d1ee22
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-25 22:53:14 +0200

    add patientDto

--- src/main/java/org/example/healthcare/dto/PatientDto.java
+++ src/main/java/org/example/healthcare/dto/PatientDto.java
@@ -1,4 +1,20 @@
 package org.example.healthcare.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class PatientDto {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private LocalDate dateNaissance;
 }
