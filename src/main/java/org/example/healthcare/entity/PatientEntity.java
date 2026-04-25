35d5a0c5aa640540776e090dd1d94760736c2fed
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-25 13:20:59 +0200

    ajout entité Patient

--- src/main/java/org/example/healthcare/entity/PatientEntity.java
+++ src/main/java/org/example/healthcare/entity/PatientEntity.java
@@ -1,4 +1,30 @@
 package org.example.healthcare.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "patient")
 public class PatientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private LocalDate dateNaissance;
+
+    @OneToMany(mappedBy = "patient",cascade = CascadeType.ALL)
+    List<RendezVousEntity> rendezVousEntities;
+
+    @OneToOne(mappedBy = "patient",cascade = CascadeType.ALL)
+    private DossierMedicalEntity dossierMedicalEntities;
+
 }
