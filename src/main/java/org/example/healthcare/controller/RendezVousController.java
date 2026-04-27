614e7798a46eb9b9d015b56a5a0ef117c71eef07
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-27 16:16:04 +0200

    ajout RendezVousController

--- src/main/java/org/example/healthcare/controller/RendezVousController.java
+++ src/main/java/org/example/healthcare/controller/RendezVousController.java
@@ -1,3 +1,45 @@
 package org.example.healthcare.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.healthcare.dto.RendezVousDto;
+import org.example.healthcare.service.RendezVousService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/rendezVous")
 public class RendezVousController {
+
+    private  final RendezVousService rendezVousService;
+
+    @PostMapping
+    public RendezVousDto creerRendezVous(@RequestBody RendezVousDto dto){
+        return rendezVousService.creerRendezVous(dto);
+    }
+
+    @PutMapping("/{id}")
+    public RendezVousDto modifierRendezVous(@PathVariable Long id){
+        return rendezVousService.modifierRendezVous(id);
+    }
+    @GetMapping
+    public List<RendezVousDto> listerRendezVous(){
+        return rendezVousService.listerRendezVous();
+    }
+    @PatchMapping("/{id}/annuler")
+    public RendezVousDto annullerRendezVous(@PathVariable Long id){
+        return rendezVousService.AnnuleRendezVous(id);
+    }
+    @GetMapping("/{id}")
+    public List<RendezVousDto> chercherPatient(@PathVariable Long patientId){
+        return rendezVousService.chercherPatient(patientId);
+    }
+    @GetMapping("/{id}")
+    public List<RendezVousDto> chercherMedecin(@PathVariable Long medecinId){
+        return rendezVousService.chercherMedecin(medecinId);
+    }
 }
+
+
