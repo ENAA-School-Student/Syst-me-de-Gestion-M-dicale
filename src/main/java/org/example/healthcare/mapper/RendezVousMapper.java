b6f6e6444630e029f618e8b1d677b70325a07cf8
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-27 11:39:54 +0200

    add rendezVousMapper

--- src/main/java/org/example/healthcare/mapper/RendezVousMapper.java
+++ src/main/java/org/example/healthcare/mapper/RendezVousMapper.java
@@ -1,5 +1,23 @@
 package org.example.healthcare.mapper;
 
+import org.example.healthcare.dto.RendezVousDto;
+import org.example.healthcare.entity.MedecinEntity;
+import org.example.healthcare.entity.RendezVousEntity;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
 public interface RendezVousMapper {
+
+    @Mapping(source = "patient.id", target="patientId")
+    @Mapping(source = "medecin",target = "medecinId")
+    RendezVousDto toDto(RendezVousEntity entity);
+
+    @Mapping(source = "patient.id", target="patientId")
+    @Mapping(source = "medecin",target = "medecinId")
+    RendezVousEntity toEntity(RendezVousDto dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(RendezVousDto dto, @MappingTarget RendezVousDto rendezVousDto);
+
 
 }
