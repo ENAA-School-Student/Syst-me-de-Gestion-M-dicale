64b7200e6c7b1e04400313c89c2c62e79bfec6a3
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-27 11:38:34 +0200

    add rendezVousdto

--- src/main/java/org/example/healthcare/dto/RendezVousDto.java
+++ src/main/java/org/example/healthcare/dto/RendezVousDto.java
@@ -1,4 +1,21 @@
 package org.example.healthcare.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.healthcare.enums.StatutRendezVous;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RendezVousDto {
+    private LocalDate dateRendezVous;
+    private StatutRendezVous statut;
+    private Long patientId;
+    private Long medecinId;
+
 }
--- src/main/java/org/example/healthcare/mapper/RendezVousMapper.java
+++ src/main/java/org/example/healthcare/mapper/RendezVousMapper.java
@@ -1,4 +1,5 @@
 package org.example.healthcare.mapper;
 
 public interface RendezVousMapper {
+
 }
