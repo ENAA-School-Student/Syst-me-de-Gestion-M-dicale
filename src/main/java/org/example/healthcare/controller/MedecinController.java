628fc41b1cf5896d880d3ca6a016ca38740fdc89
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-28 16:31:01 +0200

    update

--- src/main/java/org/example/healthcare/controller/MedecinController.java
+++ src/main/java/org/example/healthcare/controller/MedecinController.java
@@ -1,9 +1,12 @@
 package org.example.healthcare.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.healthcare.dto.MedecinDto;
 import org.example.healthcare.service.MedecinService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,21 +14,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/medecin")
 @RequiredArgsConstructor
+@Tag(name = "Gestion des Médecins")
 public class MedecinController {
 
     private final MedecinService medecinService;
 
     @PostMapping
-    public MedecinDto ajoutermedecin( @Valid @RequestBody  MedecinDto dto){
-        return medecinService.ajouterMedecin(dto);
+    public ResponseEntity<MedecinDto> ajoutermedecin(@Valid @RequestBody  MedecinDto dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(medecinService.ajouterMedecin(dto));
     }
     @PutMapping("/{id}")
-    public MedecinDto modifiermedecin( @PathVariable Long id,@Valid @RequestBody MedecinDto dto){
-        return medecinService.modifierMedecin(id,dto);
+    public ResponseEntity<MedecinDto> modifiermedecin( @PathVariable Long id,@Valid @RequestBody MedecinDto dto){
+        return ResponseEntity.ok(medecinService.modifierMedecin(id,dto));
     }
     @DeleteMapping("/{id}")
-    public void supprimerMedecin(@PathVariable Long id){
+    public ResponseEntity<Void>supprimerMedecin(@PathVariable Long id){
         medecinService.supprimerMedecin(id);
+        return ResponseEntity.noContent().build();
     }
     @GetMapping
     public List<MedecinDto> listerMedecins(){
--- src/main/java/org/example/healthcare/controller/PatientController.java
+++ src/main/java/org/example/healthcare/controller/PatientController.java
@@ -1,40 +1,52 @@
 package org.example.healthcare.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.healthcare.dto.PatientDto;
 import org.example.healthcare.service.PatientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.PDLOverrideSupported;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/patient")
 @RequiredArgsConstructor
+@Tag(name = "Gestion des Patients")
 public class PatientController {
 
    private final PatientService patientService;
     @PostMapping
-    public PatientDto ajouterPatient(@Valid @RequestBody PatientDto dto){
-        return patientService.ajouterPatient(dto);
+    @Operation(summary = "Ajouter patient")
+    public ResponseEntity<PatientDto> ajouterPatient(@Valid @RequestBody PatientDto dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(patientService.ajouterPatient(dto));
     }
 
     @PutMapping("/{id}")
-    public PatientDto modifierPatient( @PathVariable Long id,@Valid @RequestBody PatientDto dto){
-        return patientService.modifierPatient(id,dto);
+    @Operation(summary = "Modifier ptient ")
+    public ResponseEntity<PatientDto> modifierPatient( @PathVariable Long id,@Valid @RequestBody PatientDto dto){
+        return ResponseEntity.ok(patientService.modifierPatient(id,dto));
     }
     @DeleteMapping("/{id}")
-    public void supprimerPatient( @PathVariable Long id){
+    @Operation(summary = "supprimer ptient ")
+    public ResponseEntity<Void> supprimerPatient(@PathVariable Long id){
         patientService.SupprimerPatient(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
-    public List<PatientDto> ListerPatients(){
-        return patientService.ListerPatients();
+    @Operation(summary = "Lister Patients ")
+    public ResponseEntity<List<PatientDto>> ListerPatients(){
+        return ResponseEntity.ok(patientService.ListerPatients());
     }
     @GetMapping("/{id}")
-    public PatientDto consulterPatient( @PathVariable Long id){
-        return patientService.ConsulterPatient(id);
+    @Operation(summary = "Consulter patient")
+    public ResponseEntity<PatientDto> consulterPatient( @PathVariable Long id){
+        return ResponseEntity.ok(patientService.ConsulterPatient(id));
     }
 
 }
