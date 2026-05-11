9d8111d92b26f51ae15dc896a610855ee5371a20
soufiane <soufianeelbasraoui0@gmail.com>
2026-05-11 18:59:56 +0200

    add structure

--- src/main/resources/db/migration/V3__create_user_table.sql
+++ src/main/resources/db/migration/V3__create_user_table.sql
@@ -2,5 +2,5 @@ create table user (
     id BIGINT primary key  AUTO_INCREMENT,
     username varchar(255) not null,
     email varchar(255) unique NOT NULL ,
-    pawssord VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL
 )
